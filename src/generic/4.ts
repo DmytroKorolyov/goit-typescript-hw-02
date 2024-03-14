e6504b35c39aec34511975cf597b8a6e9dd91f99
DmytroKorolyov <148568611+DmytroKorolyov@users.noreply.github.com>
2024-03-14 11:07:54 +0200

    upd

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -6,18 +6,21 @@ interface ComponentProps {
   title: string;
 }
 
-class Component<T extends ComponentProps> {
-  constructor (public props: T) {
-
-  }
+class Component<T> {
+  constructor(public props: T) {}
 }
 
-class Page extends Component<ComponentProps> {
+interface PageProps {
+  title: string;
+}
+
+class Page extends Component<PageProps> {
   pageInfo() {
     console.log(this.props.title);
   }
 }
 
-export { };
+export {};
+
 
 
